8281586c1be128de081f5817b44870c040ed3ca8
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-05-07 11:27:15 -0300

    Create index.php

---
+++ HSH/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Inicio con codigo</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/uikit.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/personal.css" />
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+  </head>
+  <body class="uk-height-viewport my-background-color">
+
+    <!--COMIENZO box de login con codigo-->
+    <div class="">
+      <div class="uk-position-center my-form-box">
+          <form class="uk-form uk-padding-small"> <!--comienzo de formulario-->
+            <div class="">
+              <p class="uk-text-lead">Ingrese su codigo de acceso</p>
+            </div>
+            <div class="uk-width-1-1 uk-padding-small">
+              <input type="text" class="uk-input uk-form-large"> <!-- campo del formulario-->
+            </div>
+            <div class="uk-width-1-1 uk-padding-small">
+              <button class="uk-width-1-1 uk-button uk-button-default" onclick="validarCodigo()">ingresar</button> <!--boton de ingresar-->
+              <?php
+                private function validarCodigo()
+                {
+                  echo "hola mundo";
+                }
+              ?>
+            </div>
+          </form> <!--fin de formulario-->
+      </div>
+      <!--FIN box de login con codigo-->
+    </div>
+
+  </body>
+</html>
