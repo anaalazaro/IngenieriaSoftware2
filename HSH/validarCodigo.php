82dc580bb8b0533e5871c04bcd6a7196b9dc4294
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-05-07 12:47:51 -0300

    Update validarCodigo.php

--- HSH/validarCodigo.php
+++ HSH/validarCodigo.php
@@ -1,7 +1,7 @@
 <?php
   if (htmlspecialchars($_GET['codigo'])=="hola") {
-    echo "el codigo es correcto";
+    echo "el codigo es correcto. debe redireccionar al home del admin";
   } else {
-    echo "el codigo es incorrecto";
+    echo "el codigo es incorrecto. debe volver a la pantalla de ingresar codigo";
   }
 ?>
