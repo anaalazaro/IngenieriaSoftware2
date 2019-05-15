6880a82c81ae779f9b5d241bb6f94365c519d65b
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-05-15 09:41:39 -0300

    eliminar residencia
    ahora muestra en html la residencia antes de confirmar su eliminacion

--- HSH/pantalla-eliminar-residencia.php
+++ HSH/pantalla-eliminar-residencia.php
@@ -11,30 +11,46 @@
   <body class="uk-height-viewport my-background-color">
     <div class="uk-position-center my-form-box">
 
-      <h3 class="uk-padding">Eliminar residencia</h3>
-
-      <form action="utils/buscarPorId.php" method="post">
-        
-        <div class="uk-width-1-1 uk-padding-small">
-          <input id="id-residencia"  name="id-residencia" type="text" class="uk-input" placeholder="ID de la Residencia" required>
-        </div>
-
         <?php
           if(isset($_GET["found"]))
           {
             if ($_GET["found"] == "false") {
-              echo "<div style='color:red'>ese ID no existe</div>";
+              echo '<form action="utils/eliminarResidencia.php" method="post">
+                <h3 class="uk-padding">Eliminar residencia</h3>
+                <div class="uk-width-1-1 uk-padding-small">
+                  <input id="id-residencia"  name="id-residencia" type="text" class="uk-input" placeholder="ID de la Residencia" required>
+                </div>
+                <div style="color:red">ese ID no existe</div>
+                <div class="uk-width-1-1 uk-padding-small">
+                  <input  type="submit" value="buscar" class="uk-width-1-1 uk-button uk-button-primary"></input>
+                </div>
+              </form>';
             }
             if ($_GET["found"] == "true") {
               //codigo en html a devolver si se encontro la residencia (mostrar carta con foto e informacion de la residencia)
               //preguntar si esta seguro de eliminar la residencia
+              echo '<form action="utils/eliminarResidencia.php" method="post">
+                <h3 class="uk-padding">Esta seguro que desea eliminar esta residencia?</h3>
+                <h4>Nombre de la residencia</h2>
+                <h5>Ubicacion, Ubicacion</h5>
+                <img src="" alt="">
+                <input  type="submit" value="eliminar" class="uk-width-1-1 uk-button uk-button-primary"></input>
+                <input  type="submit" value="cancelar" class="uk-width-1-1 uk-button uk-button-primary"></input>
+              </form>';
             }
+          } else {
+            echo '<form action="utils/eliminarResidencia.php" method="post">
+              <h3 class="uk-padding">Eliminar residencia</h3>
+              <div class="uk-width-1-1 uk-padding-small">
+                <input id="id-residencia"  name="id-residencia" type="text" class="uk-input" placeholder="ID de la Residencia" required>
+              </div>
+              <div class="uk-width-1-1 uk-padding-small">
+                <input  type="submit" value="buscar" class="uk-width-1-1 uk-button uk-button-primary"></input>
+              </div>
+            </form>';
           }
         ?>
 
-        <div class="uk-width-1-1 uk-padding-small">
-          <input  type="submit" value="buscar" class="uk-width-1-1 uk-button uk-button-primary"></input>
-        </div>
       </form>
     </div>
 
