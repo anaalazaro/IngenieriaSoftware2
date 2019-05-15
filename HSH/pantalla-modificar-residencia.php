9b756e99b94b2fea0122a6bbfb1d7afdc4ee9a8e
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-05-15 12:10:47 -0300

    Update pantalla-modificar-residencia.php

--- HSH/pantalla-modificar-residencia.php
+++ HSH/pantalla-modificar-residencia.php
@@ -45,14 +45,36 @@
           //html para campos de modificacion de residencia
           echo '<form action="utils/guardarModificacionResidencia.php" method="post">
             <h3 class="uk-padding">Modificando residencia</h3>
-            <h4>Nombre de la residencia</h2>
-            <h5>Ubicacion, Ubicacion</h5>
-            <img rc="" alt="">
+
+            <div class="uk-width-1-1 uk-padding-small">
+              <label for="foto">
+                Imagenes:
+              <input id="foto" name="foto" type="file" size="20" accept="image/*" class="uk-input" >
+            <!--<button  class="uk-width-1-1 uk-button uk-button-default" type="button" tabindex="-1">Seleccionar Fotos</button>-->
+              </label>
+            </div>
+            <div class="uk-width-1-1 uk-padding-small">
+              <input id="nombre"  name="nombre" type="text" class="uk-input" placeholder="Nombre de la Residencia" >
+            </div>
+            <div class="uk-child-width-expand@s uk-padding-small" uk-grid>
+              <div class="uk-width-1-3">
+                <input id="ubicacion"type="text" name="direccion" class="uk-input" placeholder="Direccion" >
+              </div>
+              <div class="uk-width-1-3">
+                <input id="ubicacion"type="text" name="provincia" class="uk-input" placeholder="Provincia" >
+              </div>
+              <div class="uk-width-1-3">
+                <input id="ubicacion"type="text" name="pais" class="uk-input" placeholder="Pais" >
+              </div>
+            </div>
+            <div class="uk-width-1-1 uk-padding-small">
+              <input id="descripcion" name="descripcion" type="text" class="uk-input" placeholder="Breve Descripcion"  >
+            </div>
             <div class="uk-width-1-1 uk-padding-small">
               <input  name="modificacion-finalizada" type="submit" value="listo" class="uk-width-1-1 uk-button uk-button-primary"></input>
             </div>
             <div class="uk-width-1-1 uk-padding-small">
-              <input  name="cancelar-modificacion" type="submit" value="cancelar" class="uk-width-1-1 uk-button uk-button-primary"></input>
+              <input  name="cancelar-modificacion" type="submit" value="cancelar modificacion" class="uk-width-1-1 uk-button uk-button-primary"></input>
             </div>
           </form>';
         }else {
