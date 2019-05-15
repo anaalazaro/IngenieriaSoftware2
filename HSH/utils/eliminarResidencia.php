3cccc3ae22cd77cd31f7aa616085264beae606e3
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-05-15 11:30:02 -0300

    Update eliminarResidencia.php

--- HSH/utils/eliminarResidencia.php
+++ HSH/utils/eliminarResidencia.php
@@ -1,6 +1,7 @@
 <?php
 if (isset($_POST['eliminar-definitivamente'])) {
   // codigo para eliminar definitivamente de la BD
+
 }elseif (isset($_POST['cancelar-eliminacion'])) {
   // codigo para volver atras
   header('Location: ../pantalla-eliminar-residencia.php');
