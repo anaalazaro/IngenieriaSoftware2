46d418d1565375193585ceec5d40b7010cf3867f
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-05-28 11:48:46 -0300

    Create home.php

---
+++ HSH3/vistas/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/uikit.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/personal.css" />
+    <script src="../js/uikit.min.js"></script>
+    <script src="../js/uikit-icons.min.js"></script>
+  </head>
+  <body class="uk-height-viewport my-background-color">
+    <nav class="uk-navbar-container" uk-navbar>
+      <div class="uk-navbar-center">
+
+        <ul class="uk-navbar-nav">
+            <li><a href="pantalla-agregar-residencia.php">alta de residencia</a></li>
+            <li><a href="pantalla-modificar-residencia.php">modificacion de residencia</a></li>
+            <li><a href="pantalla-eliminar-residencia.php">baja de residencia</a></li>
+            <li><a href="pantalla-crear-subasta.php">crear subasta</a></li>
+            <li><a href="pantalla-cerrar-subasta.php">cerrar subasta</a></li>
+            <li><a href="pantalla-listar-residencias.php">listar residencias</a></li>
+            <li><a href="pantalla-listar-subastas.php">Listar subastas</a></li>
+        </ul>
+
+      </div>
+    </nav>
+
+    <div class="uk-align-center uk-position-center">
+      <img src="../files/hsh-logo.png"width="550" height="550" >
+    </div>
+
+  </body>
+</html>
