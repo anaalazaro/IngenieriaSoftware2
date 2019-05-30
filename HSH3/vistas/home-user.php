8ca9bf827aca5607dc1149304c419c0ab39a3571
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-05-30 17:22:42 -0300

    Create home-user.php

---
+++ HSH3/vistas/home-user.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/uikit.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/personal.css" />
+    <script src="../js/uikit.min.js"></script>
+    <script src="../js/uikit-icons.min.js"></script>
+  </head>
+  <body class="uk-height-viewport my-background-color">
+
+
+  </body>
+</html>
