b2607182d2bcab5837bee4d1444ae9f8a5e014e9
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-05-31 10:45:50 -0300

    Update funciones-fotos.php

--- HSH3/modelos/funciones-fotos.php
+++ HSH3/modelos/funciones-fotos.php
@@ -5,10 +5,8 @@
 function guardarFoto($conexion, $res_id, $files){
   if (is_uploaded_file($files["foto"]["tmp_name"])) {
     $info=getimagesize($_FILES["foto"]["tmp_name"]);
-
     $tipo = $files["foto"]["type"];
     $imagen = file_get_contents($files["foto"]["tmp_name"]);
-
     $consulta = "INSERT INTO foto(id_residencia, imagen, tipoimagen) VALUES ($res_id, $imagen, $tipo)";
     $res = mysqli_query($conexion, $consulta);
     return $res;
