34957afbbd4fcd36d6d3f36bb6a42e0f483e5f6f
anaalazaro <49758090+anaalazaro@users.noreply.github.com>
2019-06-09 21:14:49 -0300

    Update validarUsuario.php

--- HSH3/controladores/validarUsuario.php
+++ HSH3/controladores/validarUsuario.php
@@ -1,7 +1,5 @@
 <?php
-echo "aca va la logica para el inicio de sesion de un usuario, si existe e ingreso bien la informacion, debe llevarlo al home.
- no escribir todas las funciones aca, crear algun archivo en la carpeta modelos para eso";
-//header('location: ../vistas/home-user.php')
+
 include('../modelos/conexion.php');
 include("../modelos/funciones-usuarios.php");
 $conexion=conectar();
