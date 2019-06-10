71a4a9202878fab88ff415184a031a42e25d3e52
Marina Lencinas <49081672+marinaalp@users.noreply.github.com>
2019-06-10 06:59:09 -0300

    Update pantalla-listar-residencias.php
    boton eliminar y mod

--- HSH3/vistas/pantalla-listar-residencias.php
+++ HSH3/vistas/pantalla-listar-residencias.php
@@ -41,6 +41,8 @@
           <td><?php echo $row['provincia']; ?></td>
           <td><?php echo $row['ciudad']; ?></td>
           <td><?php echo $row['descripcion']; ?></td>
+          <td><a href="pantalla-modificar-residencia.php?nombre=<?php echo $row["id"]; ?>"><span uk-icon="file-edit"></span></a></td>
+		  <td><a href="pantalla-eliminar-residencia.php?nombre=<?php echo $row["id"]; ?>"><span uk-icon="trash"></span></a></td>
         <?php } ?>
         </tr>
       </tbody>
