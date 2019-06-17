4f62392929fd8c92fc224b2ab7f7cd027d4104b8
Marina Lencinas <49081672+marinaalp@users.noreply.github.com>
2019-06-17 14:05:30 -0300

    Add files via upload

---
+++ HSH3/modelos/funciones-paquetes.php
@@ -0,0 +1,28 @@
+<?php
+function hayDatos($nombre, $semana){
+	if((isset($nombre)) && !empty($nombre) &&  (isset($semana)) && !empty($semana)  ){
+		return true;
+	}else {
+		return false;
+	}
+}
+
+function existe($nombre,$semana,$conexion){
+	$existe= "SELECT * FROM paquete WHERE nombre_res='$nombre' AND semana='$semana'";
+	$resultado1= mysqli_query($conexion,$existe);
+	
+	if(mysqli_num_rows($resultado1)==0){
+		return 0;
+	}else {
+		return 1;
+	}
+}
+
+
+function guardarPaquete($conexion, $consulta){
+	return mysqli_query($conexion, $consulta);
+}
+
+
+
+?>
