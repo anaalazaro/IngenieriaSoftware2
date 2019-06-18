4ab27c90a4acb0bb2b65afbc939f155ba36f0c05
Marina Lencinas <49081672+marinaalp@users.noreply.github.com>
2019-06-17 23:13:39 -0300

    Add files via upload

---
+++ HSH3/vistas/home-admin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/uikit.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/personal.css" />
+    <script src="../js/uikit.min.js"></script>
+    <script src="../js/uikit-icons.min.js"></script>
+  </head>
+  <body class="uk-height-viewport my-background-color">
+    <nav class="uk-navbar-container" style="background-color:white" uk-navbar>
+      <div class="uk-navbar-center">
+
+        <ul class="uk-navbar-nav">
+
+            <li><a href="pantalla-modificar-residencia.php">modificacion de residencia</a></li>
+            <li><a href="pantalla-eliminar-residencia.php">baja de residencia</a></li>
+
+            <li><a href="pantalla-cerrar-subasta.php">cerrar subasta</a></li>
+            <li><a href="pantalla-listar-residencias.php">listar residencias</a></li>
+            <li><a href="pantalla-listar-subastas.php">Listar subastas</a></li>
+            <li><a href="pantalla-listar-usuarios.php">Listar usuarios</a></li>
+<li><a href="pantalla-listar-paquetes.php">Listar paquetes</a></li> 
+            <li><a href="../modelos/cerrar-sesion.php"> Cerrar Sesion</a></li>
+        </ul>
+
+      </div>
+    </nav>
+
+    <div class="uk-align-center uk-position-center">
+      <img src="../files/hsh-logo.png"width="550" height="550" >
+    </div>
+
+
+  </body>
+</html>
