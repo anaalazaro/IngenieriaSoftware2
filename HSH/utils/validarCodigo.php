a1baaaed4073c7b4477bafad8e9c9cf4e1ba4293
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-06-27 16:33:36 -0300

    borrada la carpeta HSH
    era la primer version del sistema que no usamos mas

--- HSH/utils/validarCodigo.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if (htmlspecialchars($_GET['codigo'])=="hola") {
-    echo "el codigo es correcto. debe redireccionar al home del admin";
-    header('Location: ../home.php');
-  } else {
-    echo "el codigo es incorrecto. debe volver a la pantalla de ingresar codigo";
-    header('Location: ../index.php?fallo=true');
-  }
-?>
