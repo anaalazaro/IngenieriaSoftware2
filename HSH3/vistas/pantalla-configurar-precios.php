b024dd7076054f6e4e106a77b7fb247f36f0355f
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-06-27 18:25:51 -0300

    Update pantalla-configurar-precios.php

--- HSH3/vistas/pantalla-configurar-precios.php
+++ HSH3/vistas/pantalla-configurar-precios.php
@@ -12,7 +12,17 @@
   </head>
   <body class="uk-height-viewport my-background-color">
 
-  
+    <nav class="uk-navbar-container" style="background-color:white" uk-navbar>
+      <div class="uk-navbar-left uk-padding-small">
+        <a href="../vistas/home-admin.php" class="uk-button uk-button-primary">Volver al home</a>
+      </div>
+      <div class="uk-navbar-center">
+        <ul class="uk-navbar-nav">
+            <li><a href="../vistas/home-admin.php"><img data-src="../files/hsh-logo.png"  width="150" uk-img></a></li>
+        </ul>
+      </div>
+    </nav>
+
     <?php
     include('../modelos/conexion.php');
 
@@ -27,17 +37,17 @@
         <tr>
           <th>Basico</th>
           <th>Premium</th>
-          
+
         </tr>
       </thead>
       <tbody>
         <?php while ($row = mysqli_fetch_array($result)){ ?>
-    
+
         <tr>
           <td><?php echo $row['basico']; ?></td>
 
           <td><?php echo $row['premium']; ?></td>
-         
+
         <?php } ?>
         </tr>
       </tbody>
@@ -47,7 +57,7 @@
         <a href="../modelos/funcionModificarBasico.php" class="uk-button uk-button-primary">Modificar Basico</a>
         <a href="funcionModificarPremium.php" class="uk-button uk-button-primary">Modificar Premium</a>
         <a href="home-admin.php" class="uk-button uk-button-primary">Volver</a>
-        
+
       </div>
     </div>
   </body>
