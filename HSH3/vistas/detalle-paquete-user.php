c3c5b95c5d3f6d803eee7e2266019756c8183eab
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-07-03 11:22:13 -0300

    Update detalle-paquete-user.php
    se encuentra una lista de los estados

--- HSH3/vistas/detalle-paquete-user.php
+++ HSH3/vistas/detalle-paquete-user.php
@@ -34,6 +34,20 @@
     </nav>
 
     <?php
+
+    /*  ESTADOS DE UN PAQUETE
+
+          RESERVA     -> un paquete que se encuentra disponible para ser reservado
+          RESERVADO   -> un paquete que fue adquirido en tiempo de reserva
+          SUBASTA     -> un paquete que paso el tiempo de reserva y nadie lo adquirio
+          SUBASTADO   -> un paquete que paso el tiempo de subasta y alguien lo adquirio
+          ESPERA      -> un paquete que paso el tiempo de subasta y nadie lo adquirio
+          HOTSALE     -> un paquete que estaba en espera y fue seleccionado para hotsale
+          LIQUIDADO   -> un paquete en hotsale que fue adquirido por alguien
+          CADUCADO    -> cualquier paquete cuando pasa su fecha de uso
+
+    */
+
       include("../modelos/funciones-paquetes.php");
       $id = $_GET['id'];
       $paquete = mysqli_fetch_array(getPaquetePorId($id));
