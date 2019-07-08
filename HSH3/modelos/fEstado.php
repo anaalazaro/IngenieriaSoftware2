8a349ce10ceb92d6c10a27153b4e714fa6bce293
Marina Lencinas <49081672+marinaalp@users.noreply.github.com>
2019-07-08 18:43:18 -0300

    se agregaron nombres nuevos

--- HSH3/modelos/fEstado.php
+++ HSH3/modelos/fEstado.php
@@ -17,25 +17,25 @@ function modificarEstado($conexion){
 		$dt_subasta_fin=date("Y-m-d", strtotime("$dt_subasta_inicio +3 days"));
 
 		
-		if (($estado=='SUBASTA' OR $estado=='ESPERA' OR $estado=='FINALIZADO') AND ($fecha<=$dt_subasta_inicio)){
-			$consulta3 = "UPDATE paquete SET estado='ACTIVO' WHERE id='$id'";
+		if (($estado=='SUBASTA' OR $estado=='ESPERA' OR $estado=='CADUCADO') AND ($fecha<=$dt_subasta_inicio)){
+			$consulta3 = "UPDATE paquete SET estado='RESERVA' WHERE id='$id'";
 			$result4 = mysqli_query($conexion,$consulta3);
 		
-		}elseif(($estado=='ACTIVO' OR $estado=='ESPERA' OR $estado=='FINALIZADO') AND ($fecha>=$dt_subasta_inicio AND $fecha<=$dt_subasta_fin)){
+		}elseif(($estado=='RESERVA' OR $estado=='ESPERA' OR $estado=='CADUCADO') AND ($fecha>=$dt_subasta_inicio AND $fecha<=$dt_subasta_fin)){
 			
 			$consulta = "UPDATE paquete SET estado='SUBASTA' WHERE id='$id'";
 			$result1 = mysqli_query($conexion,$consulta);
 			
 			
 			
-		}elseif(($estado=='ACTIVO' OR $estado=='SUBASTA' OR $estado=='FINALIZADO') AND ($fecha>=$dt_subasta_fin)){
+		}elseif(($estado=='RESERVA' OR $estado=='SUBASTA' OR $estado=='CADUCADO') AND ($fecha>=$dt_subasta_fin)){
 			
 			$consulta1 = "UPDATE paquete SET estado='ESPERA' WHERE id='$id'";
 			$result2 = mysqli_query($conexion,$consulta1);
 			
 			
 		}elseif($dt<=$fecha ){
-			$consulta2 = "UPDATE paquete SET estado='FINALIZADO' WHERE id='$id'";
+			$consulta2 = "UPDATE paquete SET estado='CADUCADO' WHERE id='$id'";
 			$result3 = mysqli_query($conexion,$consulta2);
 			
 		}
