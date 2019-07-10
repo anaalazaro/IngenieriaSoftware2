94b93198bdb78c65a2c7e6c7bbcee720c4b12e9b
Marina Lencinas <49081672+marinaalp@users.noreply.github.com>
2019-07-10 01:09:07 -0300

    NUEVA TABLA HISTORIAL SUBASTAS

---
+++ subastas.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-07-2019 a las 06:08:27
+-- Versión del servidor: 10.1.26-MariaDB
+-- Versión de PHP: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hsh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subastas`
+--
+
+CREATE TABLE `subastas` (
+  `id_paquete` int(11) NOT NULL,
+  `hora_puja` datetime NOT NULL,
+  `usuario` varchar(30) NOT NULL,
+  `monto_oferta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
