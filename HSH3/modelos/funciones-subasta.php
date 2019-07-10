d76f324e69a6a4f915a2b725f9953aaa88f0f9fc
Marina Lencinas <49081672+marinaalp@users.noreply.github.com>
2019-07-10 00:58:09 -0300

    funciones subasta

---
+++ HSH3/modelos/funciones-subasta.php
@@ -0,0 +1,14 @@
+<?php
+function getSubastaPorId($id,$conexion){
+	$consulta= "SELECT * FROM subastas WHERE id_paquete='$id' ";
+	$ret=mysqli_query($conexion,$consulta);
+	return $ret;
+	
+}
+function registrarPuja($id,$conexion,$puja,$usuario){
+	$nueva_puja="INSERT INTO subastas(id_paquete, hora_puja, usuario, monto_oferta) VALUES('$id',NOW(),'$usuario','$puja')";
+	$resultado=mysqli_query($conexion,$nueva_puja);
+	
+	
+}
+?>
