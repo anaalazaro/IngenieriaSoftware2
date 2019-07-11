bf3ead3ae689ed6c05d1b80fde10d59c4f87874c
lautarooleastro <40779903+lautarooleastro@users.noreply.github.com>
2019-07-11 16:32:49 -0300

    Update fEstado.php

--- HSH3/modelos/fEstado.php
+++ HSH3/modelos/fEstado.php
@@ -9,40 +9,40 @@ function modificarEstado($conexion){
 		$dt=$row['semana'];
 		//$dt_subasta = date('Y-m-d',$dt.'- 6 month') ;
 		$dt_subasta_inicio=date("Y-m-d", strtotime("$dt -6 month"));
-		
-		
-		
-		
+
+
+
+
 		$estado=$row['estado'];
 		$dt_subasta_fin=date("Y-m-d", strtotime("$dt_subasta_inicio +3 days"));
 
-		
+
 		if (($estado=='SUBASTA' OR $estado=='ESPERA' OR $estado=='CADUCADO') AND ($fecha<=$dt_subasta_inicio)){
 			$consulta3 = "UPDATE paquete SET estado='RESERVA' WHERE id='$id'";
 			$result4 = mysqli_query($conexion,$consulta3);
-		
+
 		}elseif(($estado=='RESERVA' OR $estado=='ESPERA' OR $estado=='CADUCADO') AND ($fecha>=$dt_subasta_inicio AND $fecha<=$dt_subasta_fin)){
-			
+
 			$consulta = "UPDATE paquete SET estado='SUBASTA' WHERE id='$id'";
 			$result1 = mysqli_query($conexion,$consulta);
-			
-			
-			
-		}elseif(($estado=='RESERVA' OR $estado=='SUBASTA' OR $estado=='CADUCADO') AND ($fecha>=$dt_subasta_fin)){
-			
+
+
+
+		}elseif(($estado=='RESERVA' OR $estado=='SUBASTA' OR $estado=='CADUCADO') AND ($fecha>=$dt_subasta_fin AND $fecha<=$dt)){
+
 			$consulta1 = "UPDATE paquete SET estado='ESPERA' WHERE id='$id'";
 			$result2 = mysqli_query($conexion,$consulta1);
-			
-			
+
+
 		}elseif($dt<=$fecha ){
 			$consulta2 = "UPDATE paquete SET estado='CADUCADO' WHERE id='$id'";
 			$result3 = mysqli_query($conexion,$consulta2);
-			
+
 		}
-		
-		
+
+
 	}
 
-	
+
 }
 ?>
